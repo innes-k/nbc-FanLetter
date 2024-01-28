2046d83a5b695cb575533b97b0f66bbe384b372f
innes-k <innes.kwak@gmail.com>
2024-01-28 20:41:49 +0900

    css: CommentsBox 완성

--- fan-letter/src/App.js
+++ fan-letter/src/App.js
@@ -46,21 +46,23 @@ function App() {
       <article className="CommentsBox-container">
         <ul className="CommentsBox">
           <li className="CommentsBox-inside">
-            <div className="CommentsBox-profileImg">
+            <div className="CommentsBox-profile">
               <img
                 src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/36.jpg"
                 alt=""
               ></img>
+              <div className="CommentsBox-profileContent">
+                <span>Chad Graham</span>
+                <time>23. 11. 03. 오전 08:13:18</time>
+              </div>
             </div>
-            <div className="CommentsBox-contents">
-              <div className="CommentsBox-nickname">Chad Graham</div>
-              <div className="CommentsBox-date">23. 11. 03. 오전 08:13:18</div>
-              <div className="CommentsBox-content">
+            <div className="CommentsBox-comment">
+              <span>
                 지젤 멋져요Ipsam aspernatur nostrum eos unde velit molestiae
                 dolorem. Tenetur ullam nostrum pariatur. Et in eos. Harum
                 commodi ipsa quaerat aspernatur quod dignissimos quae quidem
                 sapiente.
-              </div>
+              </span>
             </div>
           </li>
         </ul>
