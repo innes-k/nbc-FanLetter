65c497cdc856f3e8ce278f34b239146bf4cdd686
innes-k <innes.kwak@gmail.com>
2024-01-29 17:59:03 +0900

    CommentsBox 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import "App.css";
 import "reset.css";
-import styled from "styled-components";
 import Header from "components/Header.jsx";
 import AddForm from "components/AddForm.jsx";
+import CommentsBox from "components/CommentsBox";
 
 function App() {
   return (
     <>
       <Header />
       <AddForm />
-      <article className="CommentsBox-container">
-        <ul className="CommentsBox">
-          <li className="CommentsBox-inside">
-            <div className="CommentsBox-profile">
-              <img
-                src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/36.jpg"
-                alt=""
-              ></img>
-              <div className="CommentsBox-profileContent">
-                <span>Chad Graham</span>
-                <time>23. 11. 03. 오전 08:13:18</time>
-              </div>
-            </div>
-            <div className="CommentsBox-comment">
-              <span>
-                지젤 멋져요Ipsam aspernatur nostrum eos unde velit molestiae
-                dolorem. Tenetur ullam nostrum pariatur. Et in eos. Harum
-                commodi ipsa quaerat aspernatur quod dignissimos quae quidem
-                sapiente.
-              </span>
-            </div>
-          </li>
-        </ul>
-      </article>
+      <CommentsBox />
     </>
   );
 }
---
+++ src/components/CommentsBox.styles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Article = styled.article`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+export const Ul = styled.ul`
+  background-color: black;
+  width: 500px;
+  box-sizing: border-box;
+
+  color: white;
+`;
+
+export const Li = styled.li`
+  border: 1px solid white;
+  border-radius: 10px;
+  box-sizing: border-box;
+  padding: 20px;
+  margin: 20px;
+  &:hover {
+    transform: scale(1.05);
+    cursor: pointer;
+    transition: 0.3s;
+  }
+`;
+
+export const DivProfile = styled.div`
+  display: flex;
+`;
+
+export const ImgProfile = styled.img`
+  border-radius: 100%;
+  width: 50px;
+  margin-right: 20px;
+`;
+
+export const DivProfileContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const DivComment = styled.div`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  margin-left: 70px;
+  margin-top: 10px;
+
+  background-color: rgb(64, 64, 64);
+  border-radius: 10px;
+  padding: 10px;
+`;
