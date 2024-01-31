2bde2b6b3eaacc04b845755158e484379467a58d
innes-k <innes.kwak@gmail.com>
2024-01-31 14:39:11 +0900

    feature: home, detail router 생성 완료

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import "App.css";
+import "reset.css";
+import Header from "components/Header.jsx";
+import AddForm from "components/AddForm.jsx";
+import CommentsBox from "components/CommentsBox";
+
+function Home() {
+  const [fanLetters, setFanLetters] = useState([]);
+  const memberArr = ["카리나", "윈터", "닝닝", "지젤"];
+
+  const [activeButton, setActiveButton] = useState("카리나");
+  const activeButtonChangeHandler = (member) => {
+    setActiveButton(member);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:4000/Dummy")
+      .then((response) => response.json())
+      .then((json) => {
+        setFanLetters([...json]);
+        return console.log(json);
+      });
+  }, []);
+
+  return (
+    <>
+      <Header
+        memberArr={memberArr}
+        activeButton={activeButton}
+        setActiveButton={setActiveButton}
+        activeButtonChangeHandler={activeButtonChangeHandler}
+      />
+      <AddForm fanLetters={fanLetters} setFanLetters={setFanLetters} />
+      <CommentsBox
+        fanLetters={fanLetters}
+        activeButton={activeButton}
+        setActiveButton={setActiveButton}
+        activeButtonChangeHandler={activeButtonChangeHandler}
+      />
+    </>
+  );
+}
+
+export default Home;
