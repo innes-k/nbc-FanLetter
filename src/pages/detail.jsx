679a7f99b91f42982de60d5ac59a56b124ab5b37
innes-k <innes.kwak@gmail.com>
2024-02-01 16:43:41 +0900

    fix: detail 페이지 새로고침시 에러 (useEffect로 fix)

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import * as St from "./detail.styles";
 
 function Detail({ fanLetters }) {
+  // 홈으로 이동
   const navigate = useNavigate();
   const homeClick = () => {
     navigate("/");
   };
+
+  // 클릭한 fanLetter로 정보 가져오기
   const params = useParams();
 
-  const foundFanLetter = fanLetters.find((fanLetter) => {
-    return fanLetter.id === params.pageId;
-  });
+  const [selectedFanLetter, setSelectedFanLetter] = useState(null);
+  useEffect(() => {
+    const foundFanLetter = fanLetters.find((foundFanLetter) => {
+      return foundFanLetter.id === params.pageId;
+    });
+    if (foundFanLetter) {
+      setSelectedFanLetter(foundFanLetter);
+    } else {
+    }
+  }, [fanLetters, params.pageId]);
+
+  // 수정하기
 
   return (
     <>
@@ -20,22 +32,39 @@ function Detail({ fanLetters }) {
       </header>
       <St.DetailBoxFlex>
         <St.DetailBox>
-          <div className="detailBox-contents">
+          {/* <div className="detailBox-contents">
             <St.Header>
               <St.ProfileImgNickname>
                 <St.ProfileImg
-                  src={foundFanLetter.avatar}
+                  src={selectedFanLetter.avatar}
                   alt=""
                 ></St.ProfileImg>
                 <St.ProfileNickname>
-                  {foundFanLetter.nickname}
+                  {selectedFanLetter.nickname}
                 </St.ProfileNickname>
               </St.ProfileImgNickname>
-              <time>{foundFanLetter.createdAt}</time>
+              <time>{selectedFanLetter.createdAt}</time>
             </St.Header>
-            <St.MemberName>{foundFanLetter.writedTo}</St.MemberName>
-            <St.LetterContent>{foundFanLetter.content}</St.LetterContent>
-          </div>
+            <St.MemberName>{selectedFanLetter.writedTo}</St.MemberName>
+            <St.LetterContent>{selectedFanLetter.content}</St.LetterContent>
+          </div> */}
+          {selectedFanLetter ? (
+            <div className="detailBox-contents">
+              <St.Header>
+                <St.ProfileImgNickname>
+                  <St.ProfileImg src={selectedFanLetter.avatar} alt="" />
+                  <St.ProfileNickname>
+                    {selectedFanLetter.nickname}
+                  </St.ProfileNickname>
+                </St.ProfileImgNickname>
+                <time>{selectedFanLetter.createdAt}</time>
+              </St.Header>
+              <St.MemberName>{selectedFanLetter.writedTo}</St.MemberName>
+              <St.LetterContent>{selectedFanLetter.content}</St.LetterContent>
+            </div>
+          ) : (
+            <div>Loading...</div>
+          )}
           <St.BottomBtnsDiv>
             <St.BottomBtns>수정</St.BottomBtns>
             <St.BottomBtns>삭제</St.BottomBtns>
