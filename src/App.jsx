b14783cc824b19c9d70757cb23104180b3de55c8
innes-k <innes.kwak@gmail.com>
2024-02-05 11:30:42 +0900

    context 컴포넌트로 분리 (router파일 정리)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import FanLettersProvider from "components/context/FanLettersContext";
+import { GlobalStyle } from "components/styles/GlobalStyle";
 import "reset.css";
 import Router from "shared/Router";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <FanLettersProvider>
+        <Router />
+      </FanLettersProvider>
+    </>
+  );
 }
 
 export default App;
--- src/components/context/FanLettersContext.js
+++ src/components/context/FanLettersContext.js
@@ -1,3 +1,54 @@
 import { createContext } from "react";
+import { dummyData } from "shared/fakeData";
+import { useState, useEffect } from "react";
 
 export const FanLettersContext = createContext(null);
+
+const FanLettersProvider = ({ children }) => {
+  const [fanLetters, setFanLetters] = useState([dummyData]);
+  const memberArr = ["카리나", "윈터", "닝닝", "지젤"];
+
+  const [activeButton, setActiveButton] = useState("카리나");
+  const activeButtonChangeHandler = (member) => {
+    setActiveButton(member);
+  };
+
+  useEffect(() => {
+    // fetch("http://localhost:4000/Dummy")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     setFanLetters([...json]);
+    //     return console.log(json);
+    //   });
+    setFanLetters([...dummyData]);
+    console.log(dummyData);
+  }, []);
+
+  // detail 페이지의 states
+  const [isEdit, setIsEdit] = useState(false);
+  const [editedContent, setEditedContent] = useState("");
+  const [selectedFanLetter, setSelectedFanLetter] = useState(null);
+
+  return (
+    <FanLettersContext.Provider
+      value={{
+        fanLetters,
+        setFanLetters,
+        memberArr,
+        activeButton,
+        setActiveButton,
+        activeButtonChangeHandler,
+        editedContent,
+        isEdit,
+        setIsEdit,
+        setEditedContent,
+        selectedFanLetter,
+        setSelectedFanLetter,
+      }}
+    >
+      {children}
+    </FanLettersContext.Provider>
+  );
+};
+
+export default FanLettersProvider;
